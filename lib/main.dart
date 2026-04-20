553e290915c9c8ae72f6ec4c891e1e0b053510e8
hyukjunkim1116 <nmkk1234@naver.com>
2026-04-21 00:11:23 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MoisSmartLedger extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Pretendard', colorSchemeSeed: Colors.indigo),
+      theme:
+          ThemeData(fontFamily: 'Pretendard', colorSchemeSeed: Colors.indigo),
       home: const SearchPage(),
     );
   }
@@ -39,7 +40,8 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MOIS 명칭 중복 검색하기', style: TextStyle(fontWeight: FontWeight.w800)),
+        title: const Text('MOIS 명칭 중복 검색하기',
+            style: TextStyle(fontWeight: FontWeight.w800)),
         centerTitle: true,
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
@@ -75,7 +77,9 @@ class _SearchPageState extends State<SearchPage> {
     final names = _groupedResults.keys.toList();
     if (names.isEmpty) {
       return Expanded(
-        child: Center(child: Text(_searchController.text.isEmpty ? '검색어를 입력하세요' : '결과가 없습니다')),
+        child: Center(
+            child: Text(
+                _searchController.text.isEmpty ? '검색어를 입력하세요' : '결과가 없습니다')),
       );
     }
 
@@ -91,7 +95,6 @@ class _SearchPageState extends State<SearchPage> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
               side: BorderSide.none,
-
             ),
             child: ExpansionTile(
               shape: const Border(),
@@ -107,7 +110,8 @@ class _SearchPageState extends State<SearchPage> {
                   ),
                   const SizedBox(width: 4), // 제목과 배지 사이의 간격
                   Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
                     child: Text(
                       '${items.length}건',
                       style: const TextStyle(
@@ -122,7 +126,7 @@ class _SearchPageState extends State<SearchPage> {
                 int idx = entry.key;
                 ExcelRowData item = entry.value;
                 bool isLast = idx == items.length - 1; // 마지막 아이템인지 판별
-                return _buildDetail(item,isLast);
+                return _buildDetail(item, isLast);
               }).toList(),
             ),
           );
@@ -131,114 +135,118 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 
-  Widget _buildDetail(ExcelRowData item,bool isLast) {
+  Widget _buildDetail(ExcelRowData item, bool isLast) {
     // 날짜에서 'T' 이후 문자열 제거 (예: 2023-10-27T00:00:00 -> 2023-10-27)
     final displayDate = item.permitDate.contains('T')
         ? item.permitDate.split('T')[0]
         : item.permitDate;
 
-    return    SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.indigo[50], // 기존 color를 여기로 이동
-          border: isLast
-              ? null // 마지막 아이템이면 보더 없음
-              : Border(
-            bottom: BorderSide(
-              color: Colors.indigo.withOpacity(0.2), // 선 색상 (연한 인디고)
-              width: 1, // 선 두께
-            ),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 상단 영역: 유형과 날짜
+    return Expanded(
+        child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.indigo[50], // 기존 color를 여기로 이동
+                border: isLast
+                    ? null // 마지막 아이템이면 보더 없음
+                    : Border(
+                        bottom: BorderSide(
+                          color:
+                              Colors.indigo.withOpacity(0.2), // 선 색상 (연한 인디고)
+                          width: 1, // 선 두께
+                        ),
+                      ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 상단 영역: 유형과 날짜
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text(
-                  '법인성격: ',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '법인성격: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        item.type.isEmpty ? '-' : item.type,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'ㅣ',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        '허가연월일: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        displayDate,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                Text(
-                  item.type.isEmpty ? '-' : item.type,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '근거법률: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        item.law.isEmpty ? '-' : item.law,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'ㅣ',
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        '기능 및 목적: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        item.purpose.isEmpty ? '-' : item.purpose,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(width: 4),
-                const Text('ㅣ',style:  TextStyle(fontSize: 14, fontWeight: FontWeight.bold) ,),
-                const SizedBox(width: 4),
-                const Text(
-                  '허가연월일: ',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  displayDate,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text(
-                  '근거법률: ',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-
-                  ),
-                ),
-                Text(
-                  item.law.isEmpty ? '-' : item.law,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-
-                  ),
-                ),
-                const SizedBox(width: 4),
-                const Text('ㅣ',style:  TextStyle(fontSize: 14, fontWeight: FontWeight.bold) ,),
-                const SizedBox(width: 4),
-                const Text(
-                  '기능 및 목적: ',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  item.purpose.isEmpty ? '-' : item.purpose,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      )
-    );
+                ],
+              ),
+            )));
   }
-
 }
